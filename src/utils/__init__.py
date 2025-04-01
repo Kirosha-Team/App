0403778fd34a31e8b48184b07bdf453d6554e57e
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-04-01 23:40:20 +0300

    utils directory is now a package

---
+++ src/utils/__init__.py
@@ -0,0 +1,10 @@
+"""
+    NAME: utils.py
+    DESC: collection of utils for different tasks
+"""
+
+from src.utils.solutions.calculate import *
+from src.utils.solutions.camera import *
+from src.utils.solutions.create import *
+from src.utils.solutions.logger import *
+from src.utils.solutions.path import *
