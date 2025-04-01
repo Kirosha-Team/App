5de7e3f57380c1a4f8c4aa64aeab296bea7afd05
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-04-01 23:40:36 +0300

    removed frame_timestamp_ms method

--- src/utils/solutions/calculate.py
+++ src/utils/solutions/calculate.py
@@ -1,16 +1,11 @@
-import cv2
-
+from tkinter import Tk
 from src.constants import *
-
-def frame_timestamp(video_capture: cv2.VideoCapture) -> int:
-    # Get the current timestamp of the video in milliseconds
-    return int(video_capture.get(cv2.CAP_PROP_POS_MSEC))
 
 def accuracy(value: int) -> int:
     # Calculate the accuracy based on the provided value and maximum accuracy percentage
     return round(value * MAX_ACCURACY_PERCENTAGE)
 
-def screen_center(window, size_x: float, size_y: float) -> tuple[int, int]:
+def screen_center(window: Tk, size_x: float, size_y: float) -> tuple[int, int]:
     # Calculate the center position for the window based on screen dimensions and window size
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
