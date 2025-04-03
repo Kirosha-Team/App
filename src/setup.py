94ff921e4d271cc4ab27c1e022811a7a89eb3317
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-04-04 00:19:45 +0300

    Removed comments

--- src/setup.py
+++ src/setup.py
@@ -6,29 +6,21 @@ from src.utils import *
 from src.constants import *
 
 def download_model() -> None:
-    # Download the model from the specified link and save it to the asset path
     request.urlretrieve(MODEL_LINK, ASSET_PATH)
 
 def download_samples() -> None:
     gesture_recognizer_path = GESTURE_RECOGNIZER_PATH
 
-    # Open the URL containing the samples and read the data
     samples = request.urlopen(SAMPLES_LINK)
-    # Create a ZipFile object from the downloaded samples
-    archive = ZipFile(BytesIO(samples.read()))
 
-    # Extract all contents of the zip file to the specified path
+    archive = ZipFile(BytesIO(samples.read()))
     archive.extractall(gesture_recognizer_path)
 
-    # Rename the extracted folder to the datasets path
     os.rename(os.path.join(gesture_recognizer_path, 'rps_data_sample'), DATASETS_PATH)
 
 if __name__ == '__main__':
-    # Create the devices directory if it doesn't exist
     Path.create_directory(DEVICES_PATH)
-    # Create the model directory if it doesn't exist
     Path.create_directory(MODEL_PATH)
-    # Create the logs directory if it doesn't exist
     Path.create_directory(LOGS_PATH)
 
     download_model()
