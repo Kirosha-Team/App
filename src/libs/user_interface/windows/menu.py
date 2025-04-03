ac51e4808b439622d42bf04e1c41880e7e4bf07b
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-04-04 00:19:28 +0300

    Removed comments Moved assets & roots to src/libs/user_interface/assets/__init__.py

--- src/libs/user_interface/windows/menu.py
+++ src/libs/user_interface/windows/menu.py
@@ -1,103 +1,95 @@
 import time, datetime
 
-from src.constants import *
+from src.libs.user_interface.assets import *
 from src.utils import *
 
 class MainMenu:
     def __init__(self, on_button_pressed: callable, on_display_info_pressed: callable):
-        self.callback = on_button_pressed  # Store the callback function for button press
-        self.display = on_display_info_pressed  # Store the callback function for display info
+        self.callback = on_button_pressed
+        self.display = on_display_info_pressed
 
-        self.time = None  # Initialize time variable
-        self.date = None  # Initialize date variable
-        self.weather = None  # Initialize weather variable
+        self.time = None
+        self.date = None
+        self.weather = None
 
     def create(self) -> None:
-        self.window = Create.window(1000, 600)  # Create a window with specified dimensions
-        self.canvas = Create.canvas(self.window, 600, 1000)  # Create a canvas within the window
-
-        self.image_image_1 = Create.image(0, "image_1")  # Load the first image
-        self.image_image_2 = Create.image(0, "image_2")  # Load the second image
-        self.button_image_1 = Create.image(0, "button_1")  # Load the first button image
-        self.button_image_2 = Create.image(0, "button_2")  # Load the second button image
-        self.button_image_3 = Create.image(0, "button_3")  # Load the third button image
+        self.roots = create_default_window()
+        self.assets = load_main_menu_components()
 
         def update() -> None:
-            current_time = time.strftime('%H:%M')  # Get the current time in HH:MM format
+            current_time = time.strftime('%H:%M')
 
-            date = datetime.date.today()  # Get today's date
-            weekday = WEEKDAYS[datetime.date.weekday(date)]  # Get the current weekday
+            date = datetime.date.today()
+            weekday = WEEKDAYS[datetime.date.weekday(date)]
 
-            self.canvas.itemconfig(tagOrId=self.time, text=current_time)  # Update the time label on the canvas
-            self.canvas.itemconfig(tagOrId=self.date, text=f'{weekday}.\n{date.day}')  # Update the date label on the canvas
+            self.roots["canvas"].itemconfig(tagOrId=self.time, text=current_time)
+            self.roots["canvas"].itemconfig(tagOrId=self.date, text=f'{weekday}.\n{date.day}')
 
-            self.window.after(UPDATE_DELAY, update)  # Schedule the update function to run again after a delay
+            self.roots["window"].after(UPDATE_DELAY, update)
 
         Create.frame(
-            self.canvas,
+            self.roots["canvas"],
             500.0,
             300.0,
-            self.image_image_1  # Create a frame with the first image
+            self.assets["background_image"]
         )
 
         Create.frame(
-            self.canvas,
+            self.roots["canvas"],
             335.0,
             520.0,
-            self.image_image_2  # Create a frame with the second image
+            self.assets["split_box_image"]
         )
 
         Create.button(
-            self.button_image_1,
+            self.assets["gestures_button_image"],
             475.0,
             475.0,
             90.0,
             90.0,
-            lambda: self.callback(1, False, False)  # Set up button 1 to call the callback with specific parameters
+            lambda: self.callback(1, False, False)  # Open the gestures editor window
         )
 
         Create.button(
-            self.button_image_2,
+            self.assets["devices_button_image"],
             585.0,
             475.0,
             90.0,
             90.0,
-            lambda: self.callback(2, False, False)  # Set up button 2 to call the callback with specific parameters
+            lambda: self.callback(2, False, False)  # Open the devices editor window
         )
 
         Create.button(
-            self.button_image_3,
+            self.assets["info_button_image"],
             695.0,
             475.0,
             90.0,
             90.0,
-            lambda: self.display()  # Set up button 3 to call the display function
+            lambda: self.display() # Open the info window
         )
 
         self.time = Create.label(
-            self.canvas,
+            self.roots["canvas"],
             313.0,
             63.0,
             '',
-            128  # Create a label for displaying time
+            128
         )
 
         self.date = Create.label(
-            self.canvas,
+            self.roots["canvas"],
             253.0,
             486.0,
             '',
-            28  # Create a label for displaying date
+            28
         )
 
         self.weather = Create.label(
-            self.canvas,
+            self.roots["canvas"],
             364.0,
             503.0,
             '~~°C',
-            28  # Create a label for displaying weather
+            28
         )
 
-        update()  # Call the update function to start the time and date updates
-
-        self.window.mainloop()  # Start the main event loop of the window
+        update()
