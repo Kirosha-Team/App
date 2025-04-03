33ba836eebf78d8d300e7946631e760d79cc1d59
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-04-04 00:16:14 +0300

    Added docs Removed assertion method

--- src/utils/solutions/logger.py
+++ src/utils/solutions/logger.py
@@ -1,3 +1,21 @@
+"""
+    NAME: logger.py
+    DESC: solution for catching errors, warnings, debugs
+
+    CLASS LOGGER UTILS:
+        STATIC METHODS:
+            convert --> returns text with replaced $ with /n
+
+    CLASS LOGGER:
+        PRIVATE METHODS:
+            __init__ --> initializes solution
+
+        PUBLIC METHODS:
+            error --> logs error in .log file
+            warning --> logs warning in .log file
+            debug --> logs debug in .log file
+"""
+
 import logging
 
 from src.utils.solutions.path import *
@@ -11,40 +29,32 @@ class LoggerUtils:
 
 class Logger:
     def __init__(self, name: str):
-        # Ensure the name is of type string
         assert(type(name) is str)
 
-        # Create a file handler that writes log messages to a file
         self.handler = handler = logging.FileHandler(
             filename=Path.get_path_to(f'{name}.log', LOGS_PATH),
             mode='w'
         )
 
-        # Create a formatter for log messages with specific formatting
         self.formatter = logging.Formatter(FORMATTER)
 
-        # Set the formatter for the handler to define the log message format
         self.handler.setFormatter(self.formatter)
 
-        # Create a logger with the specified name and set its logging level
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        # Add the file handler to the logger
         self.logger.addHandler(handler)
 
-    def assertion(self, value: bool, message: str) -> None:
-        # Log an error message if the assertion fails
-        if not value:
-            self.logger.error(msg=LoggerUtils.convert(message))
-
     def error(self, message: str) -> None:
-        # Log an error message
-        self.logger.error(msg=LoggerUtils.convert(message))
+        self.logger.error(
+            msg=LoggerUtils.convert(message)
+        )
 
     def warning(self, message: str) -> None:
-        # Log a warning message
-        self.logger.warning(msg=LoggerUtils.convert(message))
+        self.logger.warning(
+            msg=LoggerUtils.convert(message)
+        )
 
     def debug(self, message: str) -> None:
-        # Log a debug message
-        self.logger.debug(msg=LoggerUtils.convert(message))
+        self.logger.debug(
+            msg=LoggerUtils.convert(message)
+        )
