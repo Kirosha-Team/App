d6734290ad776238601a88b9ed9b2ea684f55d50
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-04-04 00:17:16 +0300

    Updated docs Removed comments

--- src/utils/solutions/create.py
+++ src/utils/solutions/create.py
@@ -1,3 +1,19 @@
+"""
+    NAME: create.py
+    DESC: solution for managing ui components
+
+    CLASS CREATE:
+        STATIC METHODS:
+            window --> returns window
+            canvas --> returns canvas
+            label --> creates text inside canvas
+            image --> loads image from assets
+            frame --> creates image inside canvas
+            button --> creates button
+            box --> creates dialogue box
+            input_box --> creates entry
+"""
+
 from tkinter import *
 
 from src.utils.solutions.calculate import *
@@ -7,54 +23,57 @@ from src.constants import *
 class Create:
     @staticmethod
     def window(width: int, height: int) -> Tk:
-        window = Tk()  # Create a new Tkinter window instance
+        window = Tk()
 
-        x, y = screen_center(window, width, height)  # Calculate the center position for the window
+        x, y = screen_center(window, width, height)
 
-        window.geometry(f'{width}x{height}+{x}+{y}')  # Set the window size and position
-        window.resizable(False, False)  # Disable window resizing
-        # window.overrideredirect(True)  # Remove window decorations (title bar, etc.)
+        window.geometry(f'{width}x{height}+{x}+{y}')
+        window.resizable(False, False)
+        # window.overrideredirect(True)
 
-        window.focus_force()  # Force focus on the window
+        window.focus_force()
 
-        return window  # Return the created window instance
+        return window
 
     @staticmethod
     def canvas(window: Tk, height: float, width: float) -> Canvas:
         canvas = Canvas(
             window,
-            bg="#FFFFFF",  # Set background color to white
+            bg="#FFFFFF",
             height=height,
             width=width,
-            bd=0,  # No border
-            highlightthickness=0,  # No highlight thickness
-            relief="ridge"  # Set relief style to ridge
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
         )
 
-        canvas.place(x=0, y=0)  # Place the canvas in the top-left corner of the window
+        canvas.place(
+            x=0,
+            y=0
+        )
 
-        return canvas  # Return the created canvas instance
+        return canvas
 
     @staticmethod
     def label(canvas: Canvas, x: float, y: float, text: str, point: int) -> int:
          label = canvas.create_text(
             x,
             y,
-            anchor="nw",  # Anchor the text to the northwest (top-left)
+            anchor="nw",
             text=text,
-            fill="#FFFFFF",  # Set text color to white
-            font=("Inter Bold", point * -1)  # Set font style and size
+            fill="#FFFFFF",
+            font=("Inter Bold", point * -1)
          )
 
-         return label  # Return the created label instance
+         return label
 
     @staticmethod
     def image(index: int, image: str) -> Image:
-        image_path = ASSETS_PATH + f'/frame{index}/{image}.png'  # Construct the image path
+        image_path = ASSETS_PATH + f'/frame{index}/{image}.png'
 
-        if Path.exists(image_path):  # Check if the image file exists
+        if Path.exists(image_path):
             return PhotoImage(
-                file=image_path  # Return the PhotoImage instance if the file exists
+                file=image_path
             )
 
     @staticmethod
@@ -62,58 +81,58 @@ class Create:
         frame = canvas.create_image(
             x,
             y,
-            image=image  # Create an image frame on the canvas
+            image=image
         )
 
-        return frame  # Return the created frame instance
+        return frame
 
     @staticmethod
     def button(image: Image, x: float, y: float, w: float, h: float, callback=None, text=None) -> Button:
         button = Button(
             image=image,
-            borderwidth=0,  # No border width
-            highlightthickness=0,  # No highlight thickness
-            relief="flat",  # Set relief style to flat
-            command=callback,  # Set the button's command callback
-            text=text,  # Set the button's text
+            borderwidth=0,
+            highlightthickness=0,
+            relief="flat",
+            command=callback,
+            text=text,
         )
 
         button.place(
             x=x,
             y=y,
             width=w,
-            height=h  # Place the button at specified coordinates with given dimensions
+            height=h
         )
 
-        return button  # Return the created button instance
+        return button
 
     @staticmethod
     def box(category: int, title: str, text: str) -> str:
-        dialogue_box = BOXES_TYPES[category]  # Get the dialogue box type based on category
+        dialogue_box = BOXES_TYPES[category]
 
-        if dialogue_box is not None:  # Check if the dialogue box type is valid
+        if dialogue_box is not None:
             box = dialogue_box(
                 title,
-                text  # Create the dialogue box with title and text
+                text
             )
 
-            return box  # Return the created box instance
+            return box
 
     @staticmethod
     def input_box(x: float, y: float, width: float, height: float) -> Entry:
         entry = Entry(
-            bd=0,  # No border
-            bg="#404040",  # Set background color to dark gray
-            fg="#FFFFFF",  # Set text color to white
-            highlightthickness=0,  # No highlight thickness
-            font=('Inter Bold', 30)  # Set font style and size
+            bd=0,
+            bg="#404040",
+            fg="#FFFFFF",
+            highlightthickness=0,
+            font=('Inter Bold', 30)
         )
 
         entry.place(
             x=x,
             y=y,
             width=width,
-            height=height,  # Place the input box at specified coordinates with given dimensions
+            height=height,
         )
 
-        return entry  # Return the created input box instance
+        return entry
