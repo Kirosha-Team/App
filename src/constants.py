1e7e0c7c86aeca499a3ef7b46d6b0e394d4bd31b
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-04-04 00:39:27 +0300

    Removed comments

--- src/constants.py
+++ src/constants.py
@@ -1,71 +1,66 @@
 from tkinter import messagebox
 
-# Application configuration
-VERSION = '1.00.0'  # Current version of the application
+# Paths
+UTILS_PATH = 'src/utils'
+ASSETS_PATH = 'src/libs/user_interface/assets'
 
-# Defining paths for various resources
-UTILS_PATH = 'src/utils'  # Path to utility functions
-ASSETS_PATH = 'src/libs/user_interface/assets'  # Path to user interface assets
+DEVICES_PATH = 'src/libs/communicator/devices'
+MODEL_PATH = 'src/libs/gesture_recognizer/model'
+DATASETS_PATH = 'src/libs/gesture_recognizer/datasets'
 
-DEVICES_PATH = 'src/libs/communicator/devices'  # Path to device communication resources
-MODEL_PATH = 'src/libs/gesture_recognizer/model'  # Path to gesture recognition model
-DATASETS_PATH = 'src/libs/gesture_recognizer/datasets'  # Path to datasets for gesture recognition
+LOGS_PATH = 'src/logs'
+GESTURE_RECOGNIZER_PATH = 'src/libs/gesture_recognizer'
 
-LOGS_PATH = 'src/logs' # Path to logs
-GESTURE_RECOGNIZER_PATH = 'src/libs/gesture_recognizer' # Path to gesture recognizer library
+ASSET_PATH = MODEL_PATH + '/gesture_recognizer.task'
 
-# Getting the asset path for the gesture recognizer model
-ASSET_PATH = MODEL_PATH + '/gesture_recognizer.task'  # Fetching the model path using Path utility
+# Links
+MODEL_LINK = 'https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task'
+SAMPLES_LINK = 'https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/rps_data_sample.zip'
 
-# Links to the model and sample data
-MODEL_LINK = 'https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task'  # URL for the gesture recognizer model
-SAMPLES_LINK = 'https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/rps_data_sample.zip'  # URL for sample data
+# Repository parameters
+VERSION = '1.1.0'
 
-# Configuration constants for gesture recognition
-MAX_HANDS = 2  # Maximum number of hands to detect
-MIN_DETECTION_CONFIDENCE = 0.5  # Minimum confidence for detection to be considered valid
-MIN_TRACKING_CONFIDENCE = 0.5  # Minimum confidence for tracking to be considered valid
+# Recognition parameters
+MAX_HANDS = 2
+MIN_DETECTION_CONFIDENCE = 0.5
+MIN_TRACKING_CONFIDENCE = 0.5
 
-# Accuracy thresholds
-MIN_ACCURACY_PERCENTAGE = 70  # Minimum accuracy percentage for valid recognition
-MAX_ACCURACY_PERCENTAGE = 100  # Maximum accuracy percentage
+# Communication parameters
+LOCAL_PORT = 8888
+BUFFER_SIZE = 1024
 
-# Image configuration
-FLIP_CODE = 1 # Image rotation code
-MAX_IMAGES = 100  # Maximum number of images to save
-
-# Datasets configuration
-MIN_GESTURES_AMOUNT = 2 # Default datasets directory size
-DEFAULT_GESTURE = 'None'  # Default gesture when None gesture is recognized
-
-# Gesture configuration
-NO_GESTURE = 'none'  # Default gesture when no gesture is recognized
-
-# Communication configuration
 DEFAULT_COMMAND = 'ps'
 
-# Networking configuration
-LOCAL_PORT = 8888  # Port for local communication
-BUFFER_SIZE = 1024  # Buffer size for data transmission
+# Training parameters
+MIN_ACCURACY_PERCENTAGE = 70
+MAX_ACCURACY_PERCENTAGE = 100
 
-# Camera configuration
-CAMERA_INDEX = 0 # Current camera
-INIT_DELAY = 5 # Delay for camera initialization in seconds
-UPDATE_DELAY = 1  # Delay for updates in milliseconds
+# Camera parameters
+CAMERA_INDEX = 0
+INIT_DELAY = 5
+UPDATE_DELAY = 1
 
-# Menu configuration
-WEEKDAYS = ['Пн', 'Вт', 'Ср', 'Чт','Пт', 'Сб', 'Вс',]  # List of weekdays in Russian
+# Image parameters
+FLIP_CODE = 1
+MAX_IMAGES = 100
 
-# Types of message boxes for user notifications
-BOXES_TYPES = [messagebox.showinfo, messagebox.showwarning, messagebox.askyesno, messagebox.showerror]  # Different types of message boxes available
-
-# Window configuration
-DEFAULT_WIN_NAME = "Tk" # Default name of the window
-DEFAULT_WIN_INDEX = 0 # Default window to open
-
-# Gesture configuration
+# Gesture parameters
 MIN_GESTURE_NAME_LENGTH = 3
 MAX_GESTURE_NAME_LENGTH = 10
 
-# Logger configuration
+# Logger parameters
 FORMATTER = "%(name)s %(asctime)s %(levelname)s:  %(message)s"
+
+# Datasets parameters
+MIN_GESTURES_AMOUNT = 2
+
+DEFAULT_GESTURE = 'None'
+NO_GESTURE = 'none'
+
+# User interface parameters
+WEEKDAYS = ['Пн', 'Вт', 'Ср', 'Чт','Пт', 'Сб', 'Вс',]
+
+BOXES_TYPES = [messagebox.showinfo, messagebox.showwarning, messagebox.askyesno, messagebox.showerror]
+
+DEFAULT_WIN_NAME = "Tk"
+DEFAULT_WIN_INDEX = 0
