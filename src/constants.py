0226440568432ac76f0602e324661d45487929e4
kpers <k.pershin2008@gmail.com>
2026-01-24 15:45:18 +0300

    fixed version

--- src/constants.py
+++ src/constants.py
@@ -22,7 +22,7 @@ WEATHER_LINK = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&uni
 LOCATION_LINK = "http://ipinfo.io"
 
 # Repository parameters
-VERSION = "1.1.0"
+VERSION = "1.2.2"
 
 # Recognition parameters
 MAX_HANDS = 2
